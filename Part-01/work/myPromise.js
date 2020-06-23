3e611d850e107a785ebb9fbaf4dc45cb14bdfa55
GGupzhh <1248023357@qq.com>
2020-06-23 16:58:56 +0800

    update(myPromise async)

--- Part-01/work/myPromise.js
+++ Part-01/work/myPromise.js
@@ -11,7 +11,10 @@ class MyPromise{
   value = undefined
   // 失败的结果
   reason = undefined
-
+  // 成功的回调
+  onFulfilledCallback = []
+  // 失败的回调
+  onRejectedCallback = []
 
   constructor(executor) {
     executor(this.resolve, this.reject)
@@ -21,6 +24,8 @@ class MyPromise{
     if(this.state === this.penging) {
       this.state = this.fulfilled
       this.value = value
+      // 判断成功回调是否存在  存在则执行
+      while(this.onFulfilledCallback.length) this.onFulfilledCallback.shift()(this.value)
     }
   }
   // 失败时候的回调
@@ -28,6 +33,7 @@ class MyPromise{
     if (this.state === this.penging) {
       this.state = this.rejected
       this.reason = reason
+      while(this.onRejectedCallback.length) this.onRejectedCallback.shift()(this.reason)
     }
   }
 
@@ -38,6 +44,9 @@ class MyPromise{
     } else if (this.state === this.rejected) {
       // 失败的回调
       onRejectedCallback(this.reason)
+    } else {
+      this.onFulfilledCallback.push(onFulfilledCallback)
+      this.onRejectedCallback.push(onRejectedCallback)
     }
   }
   // 可以判断传入的函数是否符合规范  这里先不写
@@ -49,19 +58,21 @@ class MyPromise{
 
 const myPromise = new MyPromise(function(resolve, reject) {
   // 使其状态变成成功
-  resolve('成功')
+  setTimeout(() => {
+    resolve('成功')
+  }, 4000);
   // 使其状态变成失败
-  reject('失败')
+  // reject('失败')
 })
 
 myPromise.then(v => {
-  console.log(v)
+  console.log(1)
 }, err => {
   console.log(err)
 })
 
 myPromise.then(v => {
-  console.log(v)
+  console.log(2)
 }, err => {
   console.log(err)
 })
