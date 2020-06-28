925c58e35adda2aea5a6ce08eb6dbe070c90d23f
GGupzHH <1248023357@qq.com>
2020-06-28 19:36:12 +0800

    update(part-1/task-02/model-2)

--- Part-01/task-02/src/01-getting-started.js
+++
@@ -1,7 +0,0 @@
-// @flow
-function add(a: number, b: number) {
-  return a + b
-}
-
-add(1, 2)
-add('1', '2')
--- Part-01/task-02/src/02-types-inerence.js
+++
@@ -1,9 +0,0 @@
-// 类型推断
-//@flow
-
-function square(n) {
-  return n * n
-}
-
-square('100')
-// 当我们这样写的时候flow会出现提示  他推断我们的代码执行不能传入字符串
