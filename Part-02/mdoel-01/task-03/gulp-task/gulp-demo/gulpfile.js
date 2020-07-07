c84b9caf1926d4c4dda6042e92dfc730b8f2dd87
GGupzHH <1248023357@qq.com>
2020-07-07 22:01:42 +0800

    update(base gulp)

--- Part-02/mdoel-01/task-03/gulp-task/gulp-demo/gulpfile.js
+++ Part-02/mdoel-01/task-03/gulp-task/gulp-demo/gulpfile.js
@@ -1,12 +1,9 @@
-const { src, dest, parallel, series, watch } = require('gulp')
+const { src, dest, parallel } = require('gulp')
 
-const del = require('del')
-const browserSync = require('browser-sync')
-
-const loadPlugins = require('gulp-load-plugins')
-
-const plugins = loadPlugins()
-const bs = browserSync.create()
+const sass = require('gulp-sass')
+const babel  = require('gulp-babel')
+const swig = require('gulp-swig')
+const imagemin = require('gulp-imagemin')
 
 const data = {
   menus: [
@@ -49,106 +46,32 @@ const data = {
   date: new Date()
 }
 
-const clean = () => {
-  return del(['dist', 'temp'])
-}
 
-const style = () => {
-  return src('src/assets/styles/*.scss', { base: 'src' })
-    .pipe(plugins.sass({ outputStyle: 'expanded' }))
-    .pipe(dest('temp'))
-    .pipe(bs.reload({ stream: true }))
-}
+const style = () => src('src/assets/styles/*.scss', { base: 'src' })
+  // 将未换行的括号换行
+  .pipe(sass({ outputStyle: 'expanded' }))
+  .pipe(dest('temp'))
 
-const script = () => {
-  return src('src/assets/scripts/*.js', { base: 'src' })
-    .pipe(plugins.babel({ presets: ['@babel/preset-env'] }))
-    .pipe(dest('temp'))
-    .pipe(bs.reload({ stream: true }))
-}
+const script = () => src('src/assets/scripts/*.js', { base: 'src' })
+  // 你得告诉他用什么去处理
+  .pipe(babel({ presets: ['@babel/preset-env'] }))
+  .pipe(dest('temp'))
 
-const page = () => {
-  return src('src/*.html', { base: 'src' })
-    .pipe(plugins.swig({ data, defaults: { cache: false } })) // 防止模板缓存导致页面不能及时更新
-    .pipe(dest('temp'))
-    .pipe(bs.reload({ stream: true }))
-}
+const templ = () => src('src/**/*.html', {  base: 'src' })
+  // swig是这里用的这个模板引擎  所以需要对应的包去构建
+  .pipe(swig({ data, defaults: { cache: false } }))
+  .pipe(dest('temp'))
 
-const image = () => {
-  return src('src/assets/images/**', { base: 'src' })
-    .pipe(plugins.imagemin())
-    .pipe(dest('dist'))
-}
+const image = () => src('src/assets/images/**', {  base: 'src' })
+  .pipe(imagemin())
+  .pipe(dest())
 
-const font = () => {
-  return src('src/assets/fonts/**', { base: 'src' })
-    .pipe(plugins.imagemin())
-    .pipe(dest('dist'))
-}
+  // 因为font下面我们只需要把SVG去处理
+const font = () => src('src/assets/fonts/**', { base: 'src' })
+  .pipe(imagemin())
+  .pipe(dest('dist'))
 
-const extra = () => {
-  return src('public/**', { base: 'public' })
-    .pipe(dest('dist'))
-}
-
-const serve = () => {
-  watch('src/assets/styles/*.scss', style)
-  watch('src/assets/scripts/*.js', script)
-  watch('src/*.html', page)
-  // watch('src/assets/images/**', image)
-  // watch('src/assets/fonts/**', font)
-  // watch('public/**', extra)
-  watch([
-    'src/assets/images/**',
-    'src/assets/fonts/**',
-    'public/**'
-  ], bs.reload)
-
-  bs.init({
-    notify: false,
-    port: 2080,
-    // open: false,
-    // files: 'dist/**',
-    server: {
-      baseDir: ['temp', 'src', 'public'],
-      routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
-  })
-}
-
-const useref = () => {
-  return src('temp/*.html', { base: 'temp' })
-    .pipe(plugins.useref({ searchPath: ['temp', '.'] }))
-    // html js css
-    .pipe(plugins.if(/\.js$/, plugins.uglify()))
-    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
-    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true
-    })))
-    .pipe(dest('dist'))
-}
-
-const compile = parallel(style, script, page)
-
-// 上线之前执行的任务
-const build =  series(
-  clean,
-  parallel(
-    series(compile, useref),
-    image,
-    font,
-    extra
-  )
-)
-
-const develop = series(compile, serve)
-
+const basic = parallel(style, script, templ, image, font)
 module.exports = {
-  clean,
-  build,
-  develop
+  basic
 }
