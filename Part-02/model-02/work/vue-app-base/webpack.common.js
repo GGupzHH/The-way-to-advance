49b5d319c465f86dc4d69ab76f55e51b7b693618
GGupzHH <1248023357@qq.com>
2020-07-21 23:40:02 +0800

    update(work->webpack-dev)

---
+++ Part-02/model-02/work/vue-app-base/webpack.common.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude 哪些文件不需要loader处理
+        // include 哪些文件需要loader处理
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        exclude: '/node_modules/',
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              esModule: false,
+              limit: 10 * 1024
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'vue',
+      template: './public/index.html',
+      filename: 'index.html',
+      BASE_URL: './'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/assets/**',
+          to: 'public'
+        }, {
+          from: './public/favicon.ico',
+          to: '.'
+        }
+      ]
+    })
+  ]
+}
