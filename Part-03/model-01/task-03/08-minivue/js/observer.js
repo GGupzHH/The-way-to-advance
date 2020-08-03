ed621f422af1690d1cdbc3e66049a39dac16fd8d
GGupzhh <1248023357@qq.com>
2020-08-03 18:54:36 +0800

    update(Observer)

--- Part-03/model-01/task-03/08-minivue/js/observer.js
+++ Part-03/model-01/task-03/08-minivue/js/observer.js
@@ -15,6 +15,10 @@ class Observer {
   }
 
   defineReactive(obj, key, val) {
+    // 如果value是对象 此时也会转换为get 和 set
+    this.walk(val)
+    // 当原本在data属性中是基本类型的数据，后来改变成了复杂数据类型的时候，我们需要把新改变的值也绑定 get 和 set
+    let self = this
     Object.defineProperty(obj, key, {
       get() {
         return val
@@ -24,6 +28,7 @@ class Observer {
           return
         }
         val = newvalue
+        self.walk(newvalue)
         // 发送通知
       }
     })
